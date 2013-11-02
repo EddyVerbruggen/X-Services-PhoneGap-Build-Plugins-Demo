53700ef7347d3a2b433db367a690b51b1e347a94
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-11-02 22:26:44 +0100

    testing device feature

--- js/index.js
+++ js/index.js
@@ -37,7 +37,6 @@ var app = {
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicity call 'app.receivedEvent(...);'
   onDeviceReady: function () {
-    alert('devready');
     app.receivedEvent('deviceready');
     app.initPluginDemo();
   },
