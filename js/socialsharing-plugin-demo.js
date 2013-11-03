d2ec89e0643e3df0f4964ad5d94c62e644f446f7
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-11-03 19:43:48 +0100

    testing link and image

--- js/socialsharing-plugin-demo.js
+++ js/socialsharing-plugin-demo.js
@@ -4,7 +4,7 @@ function socialsharingDemo() {
   window.plugins.socialsharing.available(function(isAvailable) {
     if (isAvailable) {
       // use a local image from inside the www folder:
-//      window.plugins.socialsharing.share('Some text with a link http://www.google.com', 'Some subject', 'www/img/logo.png');
+      window.plugins.socialsharing.share('Some text', 'Some subject', 'www/img/logo.png', 'http://www.nu.nl');
 //      window.plugins.socialsharing.share('Some text');
 
       // alternative usage:
@@ -13,10 +13,10 @@ function socialsharingDemo() {
       // window.plugins.socialsharing.share('Some text', 'http://domain.com', '/Users/username/Library/Application Support/iPhone/6.1/Applications/25A1E7CF-079F-438D-823B-55C6F8CD2DC0/Documents/.nl.x-services.appname/pics/img.jpg');
 
       // 2) an image from the internet:
-//      window.plugins.socialsharing.share('Some text', 'http://domain.com', 'http://domain.com/image.jpg');
+//      window.plugins.socialsharing.share('Some text', "Some subject', 'http://domain.com', 'http://domain.com/image.jpg');
 
       // 3) text and link:
-      window.plugins.socialsharing.share('Some text and a link', '', '', 'http://www.nu.nl');
+//      window.plugins.socialsharing.share('Some text and a link', '', '', 'http://www.nu.nl');
     }
   });
 }
