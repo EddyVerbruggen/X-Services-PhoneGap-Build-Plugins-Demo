f7af6ea64ae0319af31052dba8e8b2188b76b5af
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-11-09 21:19:56 +0100

    Testing video

--- js/videocaptureplus-plugin-demo.js
+++ js/videocaptureplus-plugin-demo.js
@@ -1,20 +1,40 @@
 "use strict";
 
 function videoCapturePlusDemo() {
-  navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 2, duration: 4});
+  navigator.device.capture.captureVideo(
+      captureSuccess,
+      captureError,
+      {limit: 1, duration: 3});
 }
 
 function captureSuccess(mediaFiles) {
   var i, len;
   for (i = 0, len = mediaFiles.length; i < len; i++) {
     var mediaFile = mediaFiles[i];
-    navigator.notification.alert(mediaFile.name, null, mediaFile.fullPath);
+    mediaFile.getFormatData(getFormatDataSuccess, getFormatDataError);
+
+    var vid = document.createElement('video');
+    vid.id = "theVideo";
+    vid.width = "480";
+    vid.height = "360";
+    vid.controls = "controls";
+    var source_vid = document.createElement('source');
+    source_vid.id = "theSource";
+    source_vid.src = mediaFile.fullPath;
+    vid.appendChild(source_vid);
+    document.getElementById('video_container').innerHTML = '';
+    document.getElementById('video_container').appendChild(vid);
   }
 }
 
-// Called if something bad happens.
-//
+function getFormatDataSuccess(mediaFileData) {
+  document.getElementById('video_meta_container').innerHTML = JSON.stringify(mediaFileData);
+}
+
 function captureError(error) {
-  var msg = 'An error occurred during capture: ' + error.code;
-  navigator.notification.alert(msg, null, 'Uh oh!');
+  alert('An error occurred during capture: ' + error.code);
+}
+
+function getFormatDataError(error) {
+  alert('An error occurred during getFormatData: ' + error.code);
 }
