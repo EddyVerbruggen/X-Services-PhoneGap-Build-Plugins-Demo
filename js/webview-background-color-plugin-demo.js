166d144b755d3e9413808cd0651967a8f20db192
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-12-10 10:24:51 +0100

    Added iOSWebViewBackgroundColor plugin

--- js/webview-background-color-plugin-demo.js
+++ js/webview-background-color-plugin-demo.js
@@ -1,5 +1,5 @@
 "use strict";
 
 function changeWebViewColor() {
-  window.plugins.ioswebviewcolor.change('#00FF00', function(){alert('color was changed, pull the app up/down to see the effect')});
+  window.plugins.webviewcolor.change('#00FF00', function(){alert('color was changed, pull the app up/down to see the effect')});
 }
