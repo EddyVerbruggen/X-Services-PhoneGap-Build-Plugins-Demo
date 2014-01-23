422a774213c1dd45ad75b530063bbc08d5c32c81
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-01-23 20:31:09 +0100

    testing image

--- js/socialsharing-plugin-demo.js
+++ js/socialsharing-plugin-demo.js
@@ -4,11 +4,11 @@ function socialsharingDemo() {
   window.plugins.socialsharing.available(function(isAvailable) {
     if (isAvailable) {
       // use a local image from inside the www folder:
-      window.plugins.socialsharing.share('Some text', 'Some subject', null, 'http://www.nu.nl');
+//      window.plugins.socialsharing.share('Some text', 'Some subject', null, 'http://www.nu.nl');
 //      window.plugins.socialsharing.share('Some text');
 
 //      window.plugins.socialsharing.share('test', null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null, function(e){alert("success: " + e)}, function(e){alert("error: " + e)});
-
+      window.plugins.socialsharing.share(null, null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null);
       // alternative usage:
 
       // 1) a local image from anywhere else (if permitted):
