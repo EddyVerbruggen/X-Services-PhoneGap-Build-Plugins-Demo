05ec015836b1a66dc76f353e41abab72b0d935e7
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-01-30 19:49:43 +0100

    Cleanup

---
+++ js/flashlight-plugin-demo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function flashlightDemo() {
+  window.plugins.flashlight.available(function(isAvailable) {
+    if (isAvailable) {
+      window.plugins.flashlight.toggle();
+    } else {
+      alert("Flashlight not available on this device");
+    }
+  })
+}
