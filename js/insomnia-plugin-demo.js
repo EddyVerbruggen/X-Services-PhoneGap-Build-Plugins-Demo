e0add0330eb8de0c93d9541919a09d2b7f85b2fc
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-03-03 16:30:14 +0100

    Added insomnia plugin

--- js/insomnia-plugin-demo.js
+++ js/insomnia-plugin-demo.js
@@ -8,8 +8,8 @@ function insomniaDemo() {
 
   // call plugin
   if (insomniaEnabled) {
-    window.plugins.insomnia.keepAwake(function(a) {alert('keepAwake ok: ' + a)}, function(b) {alert('keepAwake nok: ' + b)});
+    window.plugins.insomnia.keepAwake(function(a) {alert('keepAwake success: ' + a)}, function(b) {alert('keepAwake error: ' + b)});
   } else {
-    window.plugins.insomnia.allowSleepAgain(function(a) {alert('allowSleepAgain ok: ' + a)}, function(b) {alert('allowSleepAgain nok: ' + b)});
+    window.plugins.insomnia.allowSleepAgain(function(a) {alert('allowSleepAgain success: ' + a)}, function(b) {alert('allowSleepAgain error: ' + b)});
   }
 }
