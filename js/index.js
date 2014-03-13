80df3e24e630a13026c5e5ae48b55464f6ddb8b9
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-03-13 15:18:25 +0100

    Testing audio player

--- js/index.js
+++ js/index.js
@@ -39,6 +39,7 @@ var app = {
   onDeviceReady: function () {
     app.receivedEvent('deviceready');
     app.initPluginDemo();
+    app.testAudioPlayer();
   },
   // Update DOM on a Received Event
   receivedEvent: function (id) {
@@ -53,6 +54,85 @@ var app = {
   },
   initPluginDemo: function () {
     document.getElementById('pluginsDemoDiv').setAttribute('style', 'display:block');
+  },
+  testAudioPlayer: function() {
+
+    alert(1);
+
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://www.x-services.nl/mindfullness-test.mp3');
+    audioElement.play();
+
+    /*
+    alert(window.plugins);
+
+    var lla = window.plugins.LowLatencyAudio;
+    alert(lla);
+    var music_mp3 = "http://www.x-services.nl/mindfullness-test.mp3";
+
+    // preload audio resource
+    lla.preloadAudio( music_mp3, music_mp3, 1, function(msg){
+        }, function(msg){
+        alert( 'error 1: ' + msg );
+        });
+
+    //    lla.preloadFX( click_sound, click_sound, function(msg){
+//    }, function(msg){
+//      alert( 'error 2: ' + msg );
+//    });
+
+    // now start playing
+    lla.play( music_mp3 );
+    //    lla.loop( music_mp3 );
+
+    // stop after 1 min
+    window.setTimeout( function(){
+      //lla.stop( click_sound );
+        lla.stop( music_mp3 );
+
+        lla.unload( music_mp3 );
+        lla.unload( click_sound );
+        }, 1000 * 60 );
+    */
+
+
+
+
+
+
+    // Audio player
+    //
+
+    //    function onSuccess() {
+      //      alert("ok");
+      //    }
+    //    function onError() {
+      //      alert("nok");
+      //    }
+    //    var my_media = new Media("http://www.x-services.nl/mindfullness-test.mp3", onSuccess, onError);
+    //    var my_media = new Media("http://www.x-services.nl/mindfullness-test.mp3", onSuccess, onError);
+    //    my_media.play({ playAudioWhenScreenIsLocked : false });
+    //    my_media.play();
+
+    /*
+    // Update media position every second
+    var mediaTimer = setInterval(function () {
+      // get media position
+        my_media.getCurrentPosition(
+            // success callback
+            function (position) {
+              if (position > -1) {
+                alert((position) + " sec");
+              }
+    },
+    // error callback
+    function (e) {
+        alert("Error getting pos=" + e);
+        }
+    );
+    }, 20000);
+    */
+
   }
 };
 
