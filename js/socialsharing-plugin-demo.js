8ec07f775859e1c547739783ac9578a5010fa303
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-03-13 21:36:22 +0100

    cleanup

--- js/socialsharing-plugin-demo.js
+++ js/socialsharing-plugin-demo.js
@@ -8,7 +8,7 @@ function socialsharingDemo() {
 //      window.plugins.socialsharing.share('Some text');
 
 //      window.plugins.socialsharing.share('test', null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null, function(e){alert("success: " + e)}, function(e){alert("error: " + e)});
-      window.plugins.socialsharing.share(null, null, 'https://www.google.nl/images/srpr/logo11w.png', null);
+      window.plugins.socialsharing.share(null, null, 'https://www.google.nl/images/srpr/logo11w.png', null, socialSharingCallback, socialSharingCallback);
       // alternative usage:
 
       // 1) a local image from anywhere else (if permitted):
@@ -24,24 +24,9 @@ function socialsharingDemo() {
 }
 
 function socialsharingSMSDemo() {
-  window.plugins.socialsharing.available(function(isAvailable) {
-    if (isAvailable) {
-      // use a local image from inside the www folder:
-//      window.plugins.socialsharing.share('Some text', 'Some subject', null, 'http://www.nu.nl');
-//      window.plugins.socialsharing.share('Some text');
+  window.plugins.socialsharing.shareViaSMS("message", "0650298955", socialSharingCallback, socialSharingCallback);
+}
 
-//      window.plugins.socialsharing.share('test', null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null, function(e){alert("success: " + e)}, function(e){alert("error: " + e)});
-      window.plugins.socialsharing.shareViaSMS("message", "0650298955", function(a){alert(a)}, function(b){alert(b)});
-      // alternative usage:
-
-      // 1) a local image from anywhere else (if permitted):
-      // window.plugins.socialsharing.share('Some text', 'http://domain.com', '/Users/username/Library/Application Support/iPhone/6.1/Applications/25A1E7CF-079F-438D-823B-55C6F8CD2DC0/Documents/.nl.x-services.appname/pics/img.jpg');
-
-      // 2) an image from the internet:
-//      window.plugins.socialsharing.share('Some text', "Some subject', 'http://domain.com', 'http://domain.com/image.jpg');
-
-      // 3) text and link:
-//      window.plugins.socialsharing.share('Some text and a link', '', '', 'http://www.nu.nl');
-    }
-  });
+function socialSharingCallback(msg) {
+  setTimeout(alert(msg), 1);
 }
